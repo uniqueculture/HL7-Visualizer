588145d6d5d7eb8a108facc48dcf0d8a34213b5e
Sergei Izvorean <me@uniqueculture.net>
2012-07-19 15:09:17 -0400

    Append parsed elements to a detached div
    To avoid DOM call all parsed elements are appended to a detached div.

--- resources/js/parser.js
+++ resources/js/parser.js
@@ -10,14 +10,17 @@ function parseMessage(content, fn) {
             character: /[\n\r]/g,
             escapeReplace: "",
             wrap: $("<div />").addClass("segment")
-        },
+        }
     ];
     // Parsed message container
-    var container = $("#parsedHl7");
+    var container = $("<div />");
     content = content.trim();
                 
     // Empty container
     container.empty();
+    
+    // Remove multiple line endings
+    content = content.replace(/[\n\r]+/g, "\r");
                 
     // Extract encoding/separator characters from the message
     parseSeparators(separators, content);
@@ -29,6 +32,9 @@ function parseMessage(content, fn) {
                 
     // Split the content into HL7 parts
     splitMessage(separators, 0, content, container);
+    
+    // Set the message to display
+    $("#parsedHl7").html(container.html());
                 
     // Set encoding charts back
     $(".segment.msh .field:eq(1) .component").text(encodingChars);
@@ -55,38 +61,48 @@ function parseSeparators(defaultSeparators, message) {
         escapeReplace: "\\F\\",
         wrap: $("<span />").addClass("field")
     });
-                
+    
+    // Parse encoding characters, if less than expected use default ones
+    var encodeFieldStart = message.indexOf(defaultSeparators[0].character);
+    var encodeFieldEnd = message.indexOf(defaultSeparators[0].character, encodeFieldStart);
+    var encodeChars = message.substring(encodeFieldStart, encodeFieldEnd - encodeFieldStart);
+    if (encodeChars.trim().length < 4) {
+        // Use default encoding chars
+        encodeChars = "^~\\&";
+    }
+
     // Repetition separator
     defaultSeparators.push({
-        character: message.charAt(5),
+        character: encodeChars.charAt(1),
         escapeReplace: "\\R\\",
         wrap: $("<span />").addClass("repetition")
     });
                 
     // Component separator
     defaultSeparators.push({
-        character: message.charAt(4),
+        character: encodeChars.charAt(0),
         escapeReplace: "\\S\\",
         wrap: $("<span />").addClass("component")
     });
                 
     // Sub-Component separator
     defaultSeparators.push({
-        character: message.charAt(7),
+        character: encodeChars.charAt(3),
         escapeReplace: "\\T\\",
         wrap: $("<span />").addClass("sub-component")
     });
                 
-    console.debug("Found the following separators: ", defaultSeparators);
+    console.log("Found the following separators: ", defaultSeparators);
 }
             
 function splitMessage(separators, separatorIndex, content, container) {
     if (separatorIndex < 0 || separatorIndex > separators.length - 1) {
         if (content.trim().length == 0) {
-            content = " ";
+            container.html("&nbsp;");
+        } else {
+            container.text(content);
         }
-                    
-        container.text(content);
+
         return;
     }
                 
@@ -102,11 +118,18 @@ function splitMessage(separators, separatorIndex, content, container) {
                     
         if (separatorIndex == 0 && element.length >= 3) {
             // Segments separator. get segment name to assing as a class
-            elementContainer.addClass(element.substring(0, 3).toLowerCase());
+            elementContainer.attr("data-name", element.substring(0, 3).toLowerCase());
+        } else if (separatorIndex == 1) {
+            // Field separator
+            if (elementIndex > 0) {
+                // Add field count element
+                var $count = $("<span />").addClass("count").text(elementIndex);
+                elementContainer.prepend($count);
+            }
         }
                     
         if (elementIndex != elements.length - 1 && typeof(separator.character) == "string") {
-            container.append(separator.character);
+            container.append($("<span />").addClass("separator").text(separator.character));
         }
     }
 }
