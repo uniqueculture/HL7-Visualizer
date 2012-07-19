9b526e46269a5e1dec40d1a53263e7aa3f730623
Sergei Izvorean <me@uniqueculture.net>
2012-07-19 15:05:41 -0400

    Definition processing based on parsed message
    Definition is now assigned based on the parsed message. Also, instead of
    processing all repetitions, first repetition is used to copy definition
    to others.
    Official HL7 tables are now used to define possible values for codded
    fields.

--- resources/js/definition.handler.js
+++ resources/js/definition.handler.js
@@ -1,7 +1,7 @@
 
 function attachDefinitionByVersion(version) {
     if (typeof(version) !== "undefined" && version.match(/\d\.\d/)) {
-        console.debug("HL7 Version: " + version);
+        console.log("HL7 Version: " + version);
     } else {
         console.error("Invalid version: " + version);
         version = "0.0"; // this should load default
@@ -17,38 +17,84 @@ function attachDefinitionByVersion(version) {
                 // Try to load default definition
                 if (!loadedDefault) {
                     loadedDefault = true;
-                    $.getJSON("resources/js/definition.default.json", attachDefinition);
+                    $.getJSON("resources/js/definition.default.json", attachDefinitionAndUi);
                 }
             }
         },
-        success: attachDefinition
+        success: attachDefinitionAndUi
     });
+}
+
+function attachDefinitionAndUi(definition) {
+    attachDefinition(definition);
+    attachUiEvents();
+}
+
+function getSegmentDefinitionByName(definition, segmentName) {
+    for (var segmentIndex in definition.segments) {
+        if (definition.segments[segmentIndex].name.toLowerCase() == segmentName.toLowerCase()) {
+            return definition.segments[segmentIndex];
+        }
+    }
+    
+    return null;
+}
+
+function getComponentDescription(values, value) {
+    console.log("Searching for component value description: " + value);
+
+    var valueIndex = -1;
+    // Search for value
+    for (var searchIndex in values) {
+        var searchValue = values[searchIndex];
+        if (searchValue.value.toLowerCase() == value.toLowerCase()) {
+            valueIndex = searchIndex;
+            break;
+        }
+    }
+    
+    return valueIndex >= 0 ? values[valueIndex].description : null;
 }
             
 function attachDefinition(definition) {
     if (definition == null) {
         return;
     }
-
-    for (var segmentIndex in definition.segments) {
-        var segment = definition.segments[segmentIndex];
-        var $segment = $(".segment."+segment.name.toLowerCase());
-        if ($segment.size() == 0) {
-            console.warn("Defined segment "+segment.name+" not found");
+    
+    var $container = $("#parsedHl7")
+    var $segments = $(".segment", $container);    
+    for (var segmentIndex = 0; segmentIndex < $segments.size(); segmentIndex++) {
+        var $segment = $($segments[segmentIndex]);
+        var segmentName = $segment.attr("data-name");
+        if (segmentName == null || segmentName.length == 0) {
             continue;
         }
-
-        for (var fieldIndex in segment.fields) {
+        
+        // Get segment definition based on name
+        var segment = getSegmentDefinitionByName(definition, segmentName);
+        if (segment == null) {
+            console.warn("Message segment "+segmentName+" not found in definition");
+            continue;
+        }
+        
+        // Assign definitions for each field
+        var $fields = $(".field", $segment);
+        for (var fieldIndex = 0; fieldIndex < $fields.size(); fieldIndex++) {
+            var $field = $($fields[fieldIndex]);
             var field = segment.fields[fieldIndex];
-            var $field = $(".field:eq("+fieldIndex+")", $segment);
-            if ($field.size() == 0) {
+            if (field == null) {
                 console.warn("Defined field with index "+fieldIndex+" in "+segment.name+" was not found");
                 continue;
             }
             
+            
+            // Set field description
+            $field.attr("data-name", field.name);
+            
             // Build XML path
             var xmlPath = [segment.name.toUpperCase(), fieldIndex];
 
+            // Attempt to populate field components based on type definition
             if (typeof field.components === "undefined") {
                 // Check if field has a data type defined
                 if (typeof field.dataType !== "undefined") {
@@ -56,79 +102,145 @@ function attachDefinition(definition) {
                     if (fieldDataType == null) {
                         console.warn("Unable to find data type "+field.dataType+" for field "+field.name);
                     } else {
-                        console.debug("Populating "+fieldDataType.components.length+" components for data type " + field.dataType);
+                        console.log("Populating "+fieldDataType.components.length+" components for data type " + field.dataType);
                         field.components = fieldDataType.components;
                     }
                 }
             }
-
+            
             if (typeof field.components === "undefined") {
+                // Field does not have any components
                 // Field always has at least one component even if not defined in definition
                 // Assign field description as component title
                 $(".component", $field).attr("title", field.name + " ["+ xmlPath.join(".") +"]");
-            } else {
-                // For each repetition we need to set same component descriptions
-                var $repetitions = $(".repetition", $field);
-                $repetitions.each(function(i) {
-                    var $repetition = $(this);
-                    for (var componentIndex in field.components) {
-                        var component = field.components[componentIndex];
-                        var $component = $(".component:eq("+componentIndex+")", $repetition);
-                        if ($component.size() == 0) {
-                            console.warn("Defined component with index "+componentIndex+" in "+segment.name+"."+ fieldIndex +" was not found");
-                            continue;
-                        }
+                continue;
+            }
+            
+            // Field has components defined
+            // Since a field can be repeated there is always one repetition
+            // Assing definition to the first repetition and copy it over to others if any
+            var $repetitions = $(".repetition", $field);
+            var $repetition = $($repetitions[0]); // first repetition
+            var $components = $(".component", $repetition);
+            for (var componentIndex = 0; componentIndex < $components.size(); componentIndex++) {
+                var $component = $($components[componentIndex]);
+                var component = field.components[componentIndex];
+                if (component == null) {
+                    console.warn("Defined component with index "+componentIndex+" in "+segment.name+"."+ fieldIndex +" was not found");
+                    continue;
+                }
 
-                        // Add component index to the path
-                        xmlPath.push(parseInt(componentIndex)+1);
-                        // Assign tooltip text
-                        $component.attr("title", field.name + " &rsaquo; " + component.name + " ["+ xmlPath.join(".") +"]");
-                    
-                        // See if component has value descriptions
-                        if (typeof(component.values) !== "undefined") {
-                            var value = $component.text().trim();
-                            console.log("Searching for component value description: " + value);
-                        
-                            var valueIndex = -1;
-                            // Search for value
-                            for (var searchIndex in component.values) {
-                                var searchValue = component.values[searchIndex];
-                                if (searchValue.value == value) {
-                                    valueIndex = searchIndex;
-                                    break;
-                                }
-                            }
-                        
-                            if (valueIndex !== -1) {
-                                $component.attr("data-popover-title", field.name + " > " + component.name);
-                                $component.attr("data-content", "<strong>"+ value +"</strong> - " + component.values[valueIndex].description);
-                            } else {
-                                console.warn("Undefined component value " + value + " for " + field.name + " > " + component.name);
-                            }
-                        }
-                        
-                        // Remove the component that was just added to allow for multiple components
-                        xmlPath.pop();
+                // Add component index to the path
+                xmlPath.push(parseInt(componentIndex)+1);
+                // Assign tooltip text
+                $component.attr("title", field.name + " > " + component.name + " ["+ xmlPath.join(".") +"]");
+                
+                // See if component has value descriptions
+                if (typeof(component.values) !== "undefined") {
+                    var value = $component.text().trim();
+                    var description = getComponentDescription(component.values, value);
+
+                    if (description != null) {
+                        $component.attr("data-content", "<strong>"+ value +"</strong> - " + description);
+                    } else {
+                        console.warn("Undefined component value " + value + " for " + field.name + " > " + component.name);
                     }
-                });
+                } else if (typeof(component.table) !== "undefined") { 
+                    // Set table attribute that would be used later to load 
+                    // and attach table definition
+                    $component.attr("data-table", component.table);
+                    // Set to empty to properly attach popover event
+                    $component.attr("data-content", "");
+                }
+
+                // Remove the component that was just added to allow for multiple components
+                xmlPath.pop();
+            }
+            
+            // Copy data from the first repetition
+            for (var repetitionIndex = 1; repetitionIndex < $repetitions.length; repetitionIndex++) {
+                var $repetitionComponents = $(".component", $repetitions[repetitionIndex]);
+                if ($repetitionComponents.length == 0) {
+                    continue;
+                }
+                
+                // For each component get attribute values from 1st repetition components
+                for (componentIndex = 0; componentIndex < Math.max($components.length, $repetitionComponents.length); componentIndex++) {
+                    var originalComponent = $components[componentIndex];
+                    var copytoComponent = $repetitionComponents[componentIndex];
+                    
+                    if (originalComponent == null || copytoComponent == null) {
+                        continue;
+                    }
+                    
+                    try {
+                        copytoComponent.setAttribute("title", originalComponent.getAttribute("title"));
+                        originalComponent.hasAttribute("data-content") ? copytoComponent.setAttribute("data-content", originalComponent.getAttribute("data-content")) : null;
+                        originalComponent.hasAttribute("data-table") ? copytoComponent.setAttribute("data-table", originalComponent.getAttribute("data-table")) : null;
+                    } catch (err) {
+                        console.error(err);
+                    }
+                }
             }
         }
     }
+}
 
+function attachUiEvents() {
+    // For each component with defined table load and attache the table
+    // This must be done before tooltip
+    $(".component[data-table]").each(function(){
+        var $component = $(this);
+        var value = $component.text().trim().toLowerCase();
+        $.getJSON("resources/js/tables/" + $component.attr("data-table") + ".json", function(table){
+            var tableValue;
+            for (var valueIndex in table.values) {
+                tableValue = table.values[valueIndex];
+                if (tableValue.value.toLowerCase() == value) {
+                    $component.attr("data-content", tableValue.description);
+                    return;
+                }
+            }
+            
+            // In case we could not find a value description
+            // remove the attribute
+            console.warn("Value description not found in [" + $component.attr("data-table") + "] for value [" + value + "]" );
+            $component.removeAttr("data-content");
+        });
+    });
+
+    
     $(".component").tooltip({
-        trigger: 'manual'
+        trigger: 'manual',
+        animation: false
     });
     
     $(".component[data-content]").popover({
-        trigger: 'manual'
+        trigger: 'manual',
+        animation: false
     }).click(function() {
         $(this).tooltip('hide');
         $(this).popover('show');
     });
     
+    $(".segment .field:first-child").click(function(){
+        $.modal($(this).parent().html(), {
+            overlayClose: true
+        });
+        
+        var $fields = $("#simplemodal-data .field");
+        console.log("Populating field names for " + $fields.size() + " fields");
+        
+        // Go thru each field and append field name as tag
+        $fields.each(function(){
+            var $field = $(this);
+            $("<span />").addClass("name").text($field.attr("data-name")).appendTo($field);
+        });
+    });
+    
     
     var lastSelected = null;
-    $(".component").hover(function() {
+    $(".component").hover(function(event) {
         $(this).tooltip('show');
         if ($(this).attr("data-content")) {
             $(this).popover('hide');
