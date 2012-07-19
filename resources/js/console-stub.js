3e7e5897f187a14dedaea306608dd657a5eab8dd
Sergei Izvorean <me@uniqueculture.net>
2012-07-19 15:12:01 -0400

    Stub console object for IE

---
+++ resources/js/console-stub.js
@@ -0,0 +1,7 @@
+!function(w) {
+  //debugger
+  if (!w.console) {
+    var c = w.console = {};
+    c.log = c.error = c.info = c.debug = c.warn = c.trace = c.dir = c.dirxml = c.group = c.groupEnd = c.time = c.timeEnd = c.assert = c.profile = c.profileEnd = function() {};
+  }
+}(window);
